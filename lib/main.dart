d11ba1b35a0b9a8fced85f041a2418f3088f9086
gitthathonor <jeong.suyoung92@gmail.com>
2022-11-14 16:50:34 +0900

    HomePage를 stateful로 고치기

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          ab(),
-          c(),
-        ],
-      ),
+    return Row(
+      children: [
+        Expanded(child: ab()),
+        Expanded(child: C_com()),
+      ],
     );
   }
 }
@@ -39,8 +37,93 @@ class ab extends StatefulWidget {
 }
 
 class _abState extends State<ab> {
+  int num = 1;
+
+  void increase() {
+    setState(() {
+      num++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: A_com(num)),
+          Expanded(child: B_com(increase)),
+        ],
+      ),
+    );
+  }
+}
+
+// 컨슈머
+class A_com extends StatelessWidget {
+  const A_com(this.num, {Key? key}) : super(key: key);
+  final int num;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        children: [
+          Text("A"),
+          Expanded(
+            child: Align(
+              child: Text(
+                "$num",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class B_com extends StatelessWidget {
+  final Function increase;
+  const B_com(this.increase, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Column(
+        children: [
+          Text("B"),
+          ElevatedButton(
+            onPressed: () {
+              increase();
+            },
+            child: Text(
+              "숫자증가",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class C_com extends StatelessWidget {
+  const C_com({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Text("C"),
+    );
   }
 }
